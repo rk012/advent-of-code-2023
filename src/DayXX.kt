888227b7bf4c2437d00e173c76fbc482cea6c29a
Rishi Kumar <rishi.kumar.2837@gmail.com>
2023-12-01 22:11:20 -0500

    Update DayXX template file

--- src/DayXX.kt
+++ src/DayXX.kt
@@ -1,15 +1,31 @@
 fun main() {
+    // Part 1
+
     fun part1(input: List<String>): Int {
         return input.size
     }
+
+
+    // Part 2
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("DayXX_test")
-    check(part1(testInput) == 1)
+    // Testing
+    val testInputPart1 = """
+        lorem
+        ipsum
+        dolor
+    """.trimIndent().lines()
+
+    val testInputPart2 = """
+        sit
+        amet
+    """.trimIndent().lines()
+
+    check(part1(testInputPart1) == 3)
+    check(part2(testInputPart2) == 2)
 
     val input = readInput("DayXX")
     part1(input).println()
